0b54a93896e64e0a6a79dd6588282f10d64c109e
Craigson <craigpick@gmail.com>
2022-08-23 20:22:03 -0400

    Add requester

---
+++ api3_qrng/src/Requester.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
+
+contract Requester is RrpRequesterV0 {
+    constructor() {}
+}
